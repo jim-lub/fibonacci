4f1c1199eb3dbe08929132943c5cce244cd61451
Jim <jim_lubbers@me.com>
2019-01-29 15:46:29 +0100

    Added fibonacci sequence finder

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ class Grid extends React.Component {
       cols: this.props.cols,
       cells: this.initializeCells(this.props)
     };
+    this.lastActive = [];
+    this.countRecursion = 0;
   }
 
   initializeCells({rows, cols}) {
@@ -29,7 +31,7 @@ class Grid extends React.Component {
     for (let r = 0; r < rows; r++) {
       for (let c = 0; c < cols; c++) {
         cells.push({
-          key: i++,
+          index: i++,
           row: r,
           col: c,
           value: 0,
@@ -41,8 +43,25 @@ class Grid extends React.Component {
     return cells;
   }
 
+  componentDidUpdate() {
+  }
+
   handleClick(cell) {
+    this.removeActiveClassFromCells();
     this.incrementRowAndColumnn(cell);
+    this.findSequences();
+  }
+
+  removeActiveClassFromCells() {
+    const cellsArr =this.state.cells.slice();
+
+    this.lastActive.forEach(cell => {
+      cellsArr[cell.index].cssClass = "";
+    });
+
+    this.lastActive = [];
+
+    this.setState({cells: cellsArr});
   }
 
   /*
@@ -51,21 +70,22 @@ class Grid extends React.Component {
   incrementRowAndColumnn({row, col}) {
     const cellsArr = this.state.cells.slice();
 
-    const cellsToIncrement = this.removeDuplicatesFromArray([...this.getKeysFromRow(row), ...this.getKeysFromColumn(col)]);
+    const cellsToIncrement = this.removeDuplicatesFromArray([...this.getIndexFromRow(row), ...this.getIndexFromColumn(col)]);
 
     cellsToIncrement.forEach(cell => {
-      cellsArr[cell.key].value = ++cell.value;
-      cellsArr[cell.key].cssClass = "cell-activated";
+      cellsArr[cell.index].value = ++cell.value;
+      cellsArr[cell.index].cssClass = "cell-activated";
     });
 
+    this.lastActive = cellsToIncrement;
     this.setState({cells: cellsArr});
   }
 
-  getKeysFromRow(row) {
+  getIndexFromRow(row) {
     return this.state.cells.filter(cell => cell.row === row);
   }
 
-  getKeysFromColumn(col) {
+  getIndexFromColumn(col) {
     return this.state.cells.filter(cell => cell.col === col);
   }
 
@@ -76,6 +96,133 @@ class Grid extends React.Component {
   /*
   * Fibonacci
   */
+  findSequences() {
+    let completed = [];
+
+    const cellsArr = this.state.cells.slice();
+
+    this.lastActive.forEach(cell => {
+      completed = [...completed, ...this.horizontal(cellsArr[cell.index]),
+                                  ...this.vertical(cellsArr[cell.index])];
+    });
+
+    console.log(completed);
+
+    this.removeDuplicatesFromArray(completed).forEach(index => {
+      cellsArr[index].value = 0;
+      cellsArr[index].cssClass = "cell-reset";
+    });
+  }
+
+  horizontal(cell, completed) {
+    const cellsArr = this.state.cells.slice();
+    let sequence = [];
+    let sequence2 = [];
+
+    this.forward(cell.index, cellsArr, sequence);
+    this.backward(cell.index, cellsArr, sequence2);
+
+    let array = this.removeDuplicatesFromArray([cell.index,...sequence, ...sequence2]);
+
+    if (array.length >= 5) {
+      return array;
+    } else {
+      return [];
+    }
+  }
+
+  vertical(cell, completed) {
+    const cellsArr = this.state.cells.slice();
+    let sequence = [];
+    let sequence2 = [];
+
+    this.down(cell.index, cellsArr, sequence);
+    this.up(cell.index, cellsArr, sequence2);
+
+    let array = this.removeDuplicatesFromArray([cell.index,...sequence, ...sequence2]);
+
+    if (array.length >= 5) {
+      return array;
+    } else {
+      return [];
+    }
+  }
+
+  forward(index, cellsArr, sequence) {
+    let current = cellsArr[index].value;
+    let next = this.getNextValue(index + 1);
+    let nextnext = this.getNextNextValue(index + 2);
+
+    if (!cellsArr[index] || current === 0 || next === 0 || nextnext === 0) return;
+
+    if (current + next === nextnext) {
+      sequence.push(index, index + 1, index + 2);
+      this.forward(index + 1, cellsArr, sequence);
+    }
+    return sequence;
+
+  }
+
+  backward(index, cellsArr, sequence) {
+    let current = cellsArr[index].value;
+    let prev = this.getPrevValue(index - 1);
+    let prevprev = this.getPrevPrevValue(index - 2);
+
+    if (!cellsArr[index] || current === 0 || prev === 0 || prevprev === 0) return;
+
+    if (current - prev === prevprev) {
+      sequence.push(index, index - 1, index - 2);
+      this.backward(index - 1, cellsArr, sequence);
+    }
+    return sequence;
+
+  }
+
+  down(index, cellsArr, sequence) {
+    let current = cellsArr[index].value;
+    let next = this.getNextValue(index + this.state.cols);
+    let nextnext = this.getNextNextValue(index + (this.state.cols * 2));
+
+    if (!cellsArr[index] || current === 0 || next === 0 || nextnext === 0) return;
+
+    if (current + next === nextnext) {
+      sequence.push(index, index + this.state.cols, index + (this.state.cols * 2));
+      this.down(index + this.state.cols, cellsArr, sequence);
+    }
+    return sequence;
+
+  }
+
+  up(index, cellsArr, sequence) {
+    let current = cellsArr[index].value;
+    let prev = this.getPrevValue(index - this.state.cols);
+    let prevprev = this.getPrevPrevValue(index - (this.state.cols * 2));
+
+    if (!cellsArr[index] || current === 0 || prev === 0 || prevprev === 0) return;
+
+    if (current - prev === prevprev) {
+      sequence.push(index, index - this.state.cols, index - (this.state.cols * 2));
+      this.up(index - this.state.cols, cellsArr, sequence);
+    }
+    return sequence;
+
+  }
+
+  getNextValue(index) {
+    return (index >= this.state.cells.length) ? 0 : this.state.cells[index].value;
+  }
+
+  getNextNextValue(index) {
+    return (index >= this.state.cells.length) ? 0 : this.state.cells[index].value;
+  }
+
+  getPrevValue(index) {
+    return (index < 0) ? 0 : this.state.cells[index].value;
+  }
+
+  getPrevPrevValue(index) {
+    return (index < 0) ? 0 : this.state.cells[index].value;
+  }
 
 
   /*
@@ -85,7 +232,7 @@ class Grid extends React.Component {
     const cells = this.state.cells.map(cell => {
       return (
         <Cell
-          key={cell.key}
+          key={cell.index}
           value={cell.value}
           cssClass={cell.cssClass}
           onClick={() => this.handleClick(cell)}
